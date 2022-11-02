3280e775222f2fdecbeea0a9abec4baba41455b6
Peter Havekes <peter@havekes.eu>
2022-11-02 12:55:00 +0100

    Basic error handling

--- app/src/main/java/eu/havekes/p1monitor/MainActivity.kt
+++ app/src/main/java/eu/havekes/p1monitor/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.res.Resources
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.Settings.System.getString
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -132,7 +133,7 @@ class MainActivity : AppCompatActivity() {
 
         try {
             if (baseUrl != null) {
-                updateCurrent(currentPower, productionPower, baseUrl, apikey,applicationContext)
+                updateCurrent(currentPower, productionPower, baseUrl, apikey,applicationContext, handler)
                 updateToday(todayPower, todayProduction, todayGas, baseUrl, apikey)
                 updateMonth(monthPower, monthProduction, monthGas, baseUrl, apikey)
                 updateFinancialToday(todayFinancialPower, todayFinancialGas, baseUrl, apikey)
@@ -163,17 +164,12 @@ class MainActivity : AppCompatActivity() {
             val monthFinancialPower: TextView = findViewById(R.id.monthFinancialPower)
             val monthFinancialGas: TextView = findViewById(R.id.monthFinancialGas)
 
-            try {
-                if (baseUrl != null) {
-                    updateCurrent(currentPower, productionPower, baseUrl,apikey, applicationContext )
-                    updateToday(todayPower, todayProduction, todayGas, baseUrl, apikey)
-                    updateMonth(monthPower, monthProduction, monthGas, baseUrl, apikey)
-                    updateFinancialToday(todayFinancialPower, todayFinancialGas, baseUrl, apikey)
-                    updateFinancialMonth(monthFinancialPower, monthFinancialGas, baseUrl, apikey)
-                }
-            } catch (e:IllegalArgumentException){
-                Log.e("error", e.toString())
-                Toast.makeText(applicationContext,"Could not communicate with API. Check settings",Toast.LENGTH_SHORT).show()
+            if (baseUrl != null) {
+                updateCurrent(currentPower, productionPower, baseUrl,apikey, applicationContext, handler )
+                updateToday(todayPower, todayProduction, todayGas, baseUrl, apikey)
+                updateMonth(monthPower, monthProduction, monthGas, baseUrl, apikey)
+                updateFinancialToday(todayFinancialPower, todayFinancialGas, baseUrl, apikey)
+                updateFinancialMonth(monthFinancialPower, monthFinancialGas, baseUrl, apikey)
             }
 
         }.also { runnable = it }, delay.toLong())
@@ -183,7 +179,7 @@ class MainActivity : AppCompatActivity() {
 
 @SuppressLint("SetTextI18n")
 @OptIn(DelicateCoroutinesApi::class)
-fun updateCurrent(currentPower: TextView, productionPower: TextView, baseUrl: String, apikey: String, applicationContext: Context) {
+fun updateCurrent(currentPower: TextView, productionPower: TextView, baseUrl: String, apikey: String, applicationContext: Context, handler: Handler) {
     // launching a new coroutine
     GlobalScope.launch(Dispatchers.Main) {
         try {
@@ -196,10 +192,12 @@ fun updateCurrent(currentPower: TextView, productionPower: TextView, baseUrl: St
             } else {
                 throw IllegalArgumentException("Communication error")
             }
-        } catch (e:IllegalArgumentException){
+        } catch (e: java.lang.Exception){
             Log.e("error", e.toString())
             Toast.makeText(applicationContext,"Could not communicate with API. Check settings",Toast.LENGTH_SHORT).show()
-
+            currentPower.text = "Error"
+            productionPower.text = "Error"
+            handler.removeCallbacksAndMessages(null);
         }
     }
 }
@@ -217,9 +215,16 @@ fun updateToday(todayPower: TextView, todayProduction: TextView, todayGas: TextV
                 todayPower.text = result.body()?.get(0)?.CONSUMPTION_DELTA_KWH.toString() + " kWh"
                 todayProduction.text = result.body()?.get(0)?.PRODUCTION_DELTA_KWH.toString() + " kWh"
                 todayGas.text = result.body()?.get(0)?.CONSUMPTION_GAS_DELTA_M3.toString() + " m3"
+            } else {
+                todayPower.text = "Error"
+                todayProduction.text = "Error"
+                todayGas.text = "Error"
             }
-        } catch (e:IllegalArgumentException){
+        } catch (e:Exception) {
             Log.e("error", e.toString())
+            todayPower.text = "Error"
+            todayProduction.text = "Error"
+            todayGas.text = "Error"
         }
     }
 }
@@ -237,9 +242,16 @@ fun updateMonth(monthPower: TextView, monthProduction:TextView, monthGas: TextVi
                 monthPower.text = result.body()?.get(0)?.CONSUMPTION_DELTA_KWH.toString() + " kWh"
                 monthProduction.text = result.body()?.get(0)?.PRODUCTION_DELTA_KWH.toString() + " kWh"
                 monthGas.text = result.body()?.get(0)?.CONSUMPTION_GAS_DELTA_M3.toString() + " m3"
+            } else {
+                monthPower.text = "Error"
+                monthProduction.text = "Error"
+                monthGas.text = "Error"
             }
-        } catch (e:IllegalArgumentException){
+        } catch (e:Exception) {
             Log.e("error", e.toString())
+            monthPower.text = "Error"
+            monthProduction.text = "Error"
+            monthGas.text = "Error"
         }
     }
 }
@@ -260,9 +272,14 @@ fun updateFinancialToday(todayFinancialPower: TextView, todayFinancialGas: TextV
                 val costsGas = result.body()?.get(0)?.CONSUMPTION_COST_GAS!!
                 val roundoffcostgas = (costsGas * 100.0).roundToInt() / 100.0
                 todayFinancialGas.text = "€" + roundoffcostgas.toString()
+            } else {
+                todayFinancialPower.text = "Error"
+                todayFinancialGas.text = "Error"
             }
-        } catch (e:IllegalArgumentException){
+        } catch (e:Exception) {
             Log.e("error", e.toString())
+            todayFinancialPower.text = "Error"
+            todayFinancialGas.text = "Error"
         }
     }
 }
@@ -285,9 +302,14 @@ fun updateFinancialMonth(monthFinancialPower: TextView, monthFinancialGas: TextV
                 val costsGas = result.body()?.get(0)?.CONSUMPTION_COST_GAS!!
                 val roundoffcostgas = (costsGas * 100.0).roundToInt() / 100.0
                 monthFinancialGas.text = "€" + roundoffcostgas.toString()
+            } else {
+                monthFinancialPower.text = "Error"
+                monthFinancialGas.text = "Error"
             }
-        } catch (e:IllegalArgumentException){
+        } catch (e:Exception) {
             Log.e("error", e.toString())
+            monthFinancialPower.text = "Error"
+            monthFinancialGas.text = "Error"
         }
     }
 }
